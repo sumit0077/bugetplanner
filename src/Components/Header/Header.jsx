d90b3bba5d14eba5e0352eb32e6b9715150f1635
Sumit kumar <xperia7048@gmail.com>
2024-05-26 22:52:43 +0530

    Add files via upload

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () =>{
+    return(
+        <>
+        <div className="bg-pink-800 flex justify-center h-14 items-center">
+            <p className="text-2xl font-serif font-bold text-white">AN EXPENSE TRACKER</p>
+        </div>
+        </>
+    )
+}
+
+export default Header;
